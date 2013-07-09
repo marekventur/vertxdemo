322a75a23f8092b696d0aa51d2e214497e57f159
Marek <mail@marekventur.de>
2013-07-09 23:09:40 +0100

    Add http server

--- src/main/java/de/marekventur/vertx/demo/MainVerticle.java
+++ src/main/java/de/marekventur/vertx/demo/MainVerticle.java
@@ -1,11 +1,15 @@
 package de.marekventur.vertx.demo;
 
+import org.vertx.java.core.json.JsonObject;
 import org.vertx.java.deploy.Verticle;
 
 public class MainVerticle extends Verticle {
 
   @Override
   public void start() throws Exception {
+    JsonObject httpServerConfig = new JsonObject("{\"port\":8889,\"host\":\"localhost\"}");
+    container.deployModule("vertx.web-server-v1.0", httpServerConfig);
+
     container.deployVerticle("de.marekventur.vertx.demo.verticles.HttpHelloWorld");
   }
 
